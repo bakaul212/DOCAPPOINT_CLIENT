4c2fcfbde9c7c99643ed4997da552615d9ad58fb
HASAN <m.h.bakaul@kuwaitarmy.gov.kw>
2026-05-20 21:41:32 +0600

    protected

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+import { LoadingSpinner } from './LoadingSpinner';
+
+export const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, loading } = useAuth();
+  if (loading) return <LoadingSpinner />;
+  if (!isAuthenticated) return <Navigate to="/login" />;
+  return children;
+};
